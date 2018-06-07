bc57d37cce22330a40cbdb9c192048eadc8a4d2b
Joseph Senders <jasenders@gmail.com>
2018-06-07 13:35:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,6 @@ end
 def retrieve_element_from_index(array, index_number)
 array = ["Tacos", "Pizza", "Pasta", "Burgers"]
 index_number = [2]
-array.index_number["Pasta"]
 end
 
 def retrieve_first_element_from_array(array)
